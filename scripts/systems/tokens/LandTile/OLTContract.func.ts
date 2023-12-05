6f6e580c9d82f928f35659a3490e3cab1aea43c2
enixlee <405284695@qq.com>
2023-12-05 16:09:42 +0800

    add nft func

---
+++ scripts/systems/tokens/LandTile/OLTContract.func.ts
@@ -0,0 +1,60 @@
+import hre, { ethers } from "hardhat";
+
+const FuncConfig = {
+  sepolic: {
+    nftName: "OsairoIslandTileDynamicNFT",
+    ccipDest: "",
+    nft: "0x41E7bD6256F8Ff51966a7FDCDbe93585e5315BfA",
+  },
+  default: {
+    nftName: "OsairoIslandTileDynamicNFT",
+    ccipDest: "",
+    nft: "",
+  },
+};
+
+async function getContract() {
+  const contract = await ethers.getContractAt(config.nftName, config.nft);
+  const [owner] = await ethers.getSigners();
+
+  return contract.connect(owner);
+}
+
+async function grantRole() {
+  const contract = await getContract();
+
+  const tx = await contract.grantRole(
+    ethers.utils.id("MINTER_ROLE"),
+    config.ccipDest
+  );
+  const receipt = await tx.wait();
+  console.log(receipt);
+  console.log("Grant role ", "done!");
+}
+
+let config;
+async function main() {
+  const network = hre.network.config;
+  config =
+    network.chainId === 11155111 ? FuncConfig.sepolic : FuncConfig.default;
+
+  if (
+    !config ||
+    config.nftName === "" ||
+    config.nft === "" ||
+    config.ccipDest === ""
+  ) {
+    throw new Error("params error.");
+  }
+
+  await grantRole();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
